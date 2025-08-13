a21c0fe0f5dd3ca27835140cbf36ede2bec35b5d
HM <hieumdd@gmail.com>
2025-08-13 09:57:08 +0700

    fix: Fix basePath & assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from 'next';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const repo = 'jafar';
 
 const nextConfig: NextConfig = {
     /* config options here */
-    assetPrefix: `/${repo}/`,
-    basePath: `/${repo}`,
+    assetPrefix: isProduction ? `/${repo}/` : '',
+    basePath: isProduction ? `/${repo}` : '',
     reactStrictMode: true,
     output: 'export',
     images: { unoptimized: true },
